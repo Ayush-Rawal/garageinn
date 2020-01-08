5f59a9c8debd2fbd9fa5f8274c9483ff98889360
Ayush Rawal <31813648+Ayush-Rawal@users.noreply.github.com>
2020-01-08 21:33:00 +0530

    Add testimonial html to component

---
+++ components/testimonial.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+
+export default function(props) {
+	return (
+		<div class="item active">
+		<div class="card card-testimonial card-plain">
+			<div class="card-avatar">
+				<a href="#pablo">
+					<img class="img" src="assets/img/faces/kendall.jpg" />
+				</a>
+			</div>
+
+			<div class="card-content">
+				<h5 class="card-description">"I speak yell scream directly at the old guard on behalf of the future. I gotta say at that time I’d like to meet Kanye I speak yell scream directly at the old guard on behalf of the future. My brother Chance!!! <br/>Thank you for letting me work on this masterpiece. One of my favorite people."
+				</h5>
+				<h4 class="card-title">Cheche hasmukhlal</h4>
+				<h6 class="category text-muted">CEO @ Marketing Digital Ltd.</h6>
+			</div>
+		</div>
+	</div>
+	)
+}
